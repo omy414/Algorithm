762f90262fa69061a7bddf14bd8d8514214b1681
omy414 <omy414@naver.com>
2023-04-05 21:05:57 +0900

    fruitSeller push

--- src/main/java/FruitSeller.java
+++ src/main/java/FruitSeller.java
@@ -4,6 +4,14 @@ import java.util.*;
 
 public class FruitSeller {
 	
+	
+	/* 2023-04-05
+	 * 프로그래머스
+	 * java 과일 장수
+	 * Level 1
+	 * lessons ( 135808 )
+	 * 
+	 */
 	public static void main(String[] args) {
 		System.out.println(solution(3, 4, new int[] {1, 2, 3, 1, 2, 3, 1}));
 	}
