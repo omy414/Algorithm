481e5645042ca976707f7ddae6855ff11c51d5da
omy414 <omy414@naver.com>
2023-05-22 10:07:44 +0900

    Rectangle push

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,43 @@
+package main.java;
+
+
+import java.util.*;
+
+public class Rectangle {
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][] {{60,50},{30,70},{60,30},{80,40}}));
+	}
+	
+	public static int solution(int[][] sizes) {
+        int answer = 0;
+        
+        int maxW = 0; 
+        int maxH = 0;
+        
+        for (int i = 0; i < sizes.length; i++) {
+			int size[] = sizes[i];
+			
+			int w = size[0];
+			int h = size[1];
+			
+			if(h > w) {
+				if(h > maxW) {
+					maxW = h;
+				}
+				if(w > maxH) {
+					maxH = w;
+				}
+			}else {
+				if(h > maxH) {
+					maxH = h;
+				}
+				if(w > maxW) {
+					maxW = w;
+				}
+			}
+		}
+        System.out.println("맥스? : " +  maxW + " / " + maxH);
+        answer = (maxW*maxH);
+        return answer;
+    }
+}
