0d23d5b164dd87e9be1576fb4f9484ac2ed6513a
omy414 <omy414@naver.com>
2023-06-04 12:48:40 +0900

    add massage YinYang

--- src/main/java/AddYinYang.java
+++ src/main/java/AddYinYang.java
@@ -3,6 +3,14 @@ package main.java;
 import java.util.*;
 
 public class AddYinYang {
+	
+	/* 2023-06-04
+	 * 프로그래머스
+	 * java 음양더하기
+	 * Level 1
+	 * lessons ( 86051 )
+	 * 
+	 */
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] {4,7,12}, new boolean[] {true,false,true}));
 	}
