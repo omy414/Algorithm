344a0627a892b27f8f32b2a592854efde045b64f
omy414 <omy414@naver.com>
2023-06-12 18:24:43 +0900

    add massage PickNumberAdd

--- src/main/java/PickNumberAdd.java
+++ src/main/java/PickNumberAdd.java
@@ -4,10 +4,19 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class PickNumberAdd{
+	
+	/* 2023-06-12
+	 * 프로그래머스
+	 * java 두개 뽑아서 더하기
+	 * Level 1 
+	 * lessons ( 68644 )
+	 * 
+	 */
 	public static void main(String[] args) {
 		System.out.println(solution2(new int[] {2,1,3,4,1}));
 	}
 	
+	/* 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요. */
 	public static int[] solution(int[] numbers) {
         ArrayList<Integer> arrList = new ArrayList<Integer>();
         
