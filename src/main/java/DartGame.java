12c89958c5a3a92c154fdd29489da3a81b4be652
omy414 <omy414@naver.com>
2023-07-10 15:58:51 +0900

    dartGame developing

--- src/main/java/DartGame.java
+++ src/main/java/DartGame.java
@@ -4,15 +4,78 @@ import java.util.*;
 
 public class DartGame {
 	public static void main(String[] args) {
-		System.out.println(solution("1S2D*3T"));
+		System.out.println(solution("1D2S#10S"));
 	}
 	
 	public static int solution(String dartResult) {
-		String[] temp = dartResult.split("^[a-zA-Z]*$");
-		System.out.println(temp[0]);
         int answer = 0;
-        StringTokenizer st = new StringTokenizer(dartResult, "[a-zA-Z]*$");
-        System.out.println(st.nextToken());
+        
+        String aa = "1A";
+        
+        //System.out.println(Integer.parseInt(aa));
+        //System.out.println(aa.matches("[0-9]"));
+        
+        int score = 0;
+        
+        int sum = 0;
+        
+        int[] list = new int[3];
+        
+        for (int i = 0; i < dartResult.length(); i++) {
+			char temp = dartResult.charAt(i);
+			System.out.println(String.valueOf(temp));
+			
+			System.out.println(String.valueOf(temp).matches("[0-9]"));
+			
+			if(String.valueOf(temp).matches("[0-9]")) {
+				if(i != 0) {
+					list[sum] = score;
+					sum++;
+					//sum+=score;
+				}
+				score = Integer.parseInt(String.valueOf(temp));
+			}
+			
+			if(String.valueOf(temp).matches("[A-Z]")) {
+				if("S".equals(String.valueOf(temp))) {
+					
+				}else if("D".equals(String.valueOf(temp))) {
+					score = score * score;
+				}else if("T".equals(String.valueOf(temp))) {
+					score = score * score * score;
+				}
+			}
+			
+			if("*".equals(String.valueOf(temp))) {
+				score = score * 2;
+				
+				System.out.println("sum : " + sum);
+				System.out.println("무슨값? : " + list[sum]);
+				if(sum != 0) {
+					if(list[sum-1] != 0) {
+						list[sum-1] = list[sum-1]*2;
+					}
+				}
+				
+			}else if("#".equals(String.valueOf(temp))){
+				score = score * (-1);
+			}
+			
+			if(i == (dartResult.length() -1)) {
+				list[sum] = score;
+				//sum+=score;
+			}
+		}
+        System.out.println(list[0]);
+        System.out.println(list[1]);
+        System.out.println(list[2]);
+        
+        sum = 0;
+        for (int i = 0; i < list.length; i++) {
+			sum += list[i];
+		}
+        System.out.println("합계 : " + sum);
+        
         
         return answer;
     }
